3ba0442e07837843cd2ae7cc465d5db550edb8ae
Rachit Patel <raj4rachit@gmail.com>
2024-09-18 14:57:53 +0800

    url changes

--- public/docs/swagger-initializer.js
+++ public/docs/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://api.rachitpatel.com/docs/api-docs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
