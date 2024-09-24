b1c073345dc5ff5968aa26e219e40eb08d2d0327
Rachit Patel <rachit@technobrains.io>
2024-09-24 23:39:22 +0530

    patient data

--- routes/v1/patient.php
+++ routes/v1/patient.php
@@ -7,9 +7,9 @@ use Modules\V1\Patient\Controllers\PatientController;
 use Modules\V1\User\Controllers\RoleController;
 
 
-Route::get('/', [PatientController::class, 'index']); // List all patients
-Route::post('/', [PatientController::class, 'store']); // Create a new patient
-Route::get('/{id}', [PatientController::class, 'show']); // Show a specific patient
-Route::put('/{id}', [PatientController::class, 'update']); // Update a specific patient
-Route::delete('/{id}', [PatientController::class, 'destroy']); // Delete a specific patient
+Route::get('/', [PatientController::class, 'index'])->name('patient.index'); // List all patients
+Route::post('/', [PatientController::class, 'store'])->name('patient.store'); // Create a new patient
+Route::get('/{id}', [PatientController::class, 'show'])->name('patient.show'); // Show a specific patient
+Route::put('/{id}', [PatientController::class, 'update'])->name('patient.update'); // Update a specific patient
+Route::delete('/{id}', [PatientController::class, 'destroy'])->name('patient.destroy'); // Delete a specific patient
 
