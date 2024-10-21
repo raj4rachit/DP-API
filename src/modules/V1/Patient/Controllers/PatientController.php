7dd06d5d3b4a856fe1b55b79ddc729b5a6e0b724
Rachit Patel <rachit@technobrains.io>
2024-10-21 10:43:42 +0530

    Changes

--- src/modules/V1/Patient/Controllers/PatientController.php
+++ src/modules/V1/Patient/Controllers/PatientController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Modules\V1\Patient\Models\Patient;
 use Modules\V1\Patient\Models\PatientMedicalHistory;
+use Modules\V1\Patient\Requests\PatientCreateRequest;
 use Modules\V1\Patient\Resources\PatientResource;
 use Modules\V1\User\Models\Role;
 use Modules\V1\User\Models\User;
@@ -24,19 +25,35 @@ final class PatientController extends Controller
 {
     /**
      * @OA\Get(
-     *     path="/patient/",
+     *     path="/patient",
      *     summary="List all patients",
      *     operationId="listPatients",
      *     tags={"Patients"},
+     *     description="Retrieve a list of all patients.",
      *
      *     @OA\Response(
      *         response=200,
      *         description="List of patients",
-     *
-     *         @OA\JsonContent(type="array", @OA\Items(ref="#/components/schemas/PatientResource"))
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Patients data retrieved successfully"),
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="statusCode", type="integer", example=200),
+     *             @OA\Property(property="data", type="array",
+     *                 @OA\Items(ref="#/components/schemas/PatientResource")
+     *             )
+     *         )
      *     ),
+     *
+     *     @OA\Response(response=401, ref="#/components/responses/401"),
+     *     @OA\Response(response=422, ref="#/components/responses/422"),
+     *     @OA\Response(response=500, ref="#/components/responses/500"),
+     *
+     *     security={
+     *         {"bearerAuth": {}}
+     *     }
      * )
      */
+
     public function index(): JsonResponse
     {
         $patients = Patient::with('user', 'medicalHistories')->get();
@@ -45,56 +62,66 @@ final class PatientController extends Controller
 
     /**
      * @OA\Post(
-     *     path="/patient/",
+     *     path="/patient",
      *     summary="Create a new patient",
      *     operationId="createPatient",
      *     tags={"Patients"},
+     *     security={{"bearerAuth":{}}},
+     *     description="Create a new patient with the provided information.",
      *
      *     @OA\RequestBody(
      *         required=true,
-     *
-     *         @OA\JsonContent(ref="#/components/schemas/PatientCreateRequest")
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 @OA\Property(property="first_name", type="string", description="Patient's first name"),
+     *                 @OA\Property(property="last_name", type="string", description="Patient's last name"),
+     *                 @OA\Property(property="gender", type="string", description="Patient's gender"),
+     *                 @OA\Property(property="dob", type="string", format="date", description="Date of birth (YYYY-MM-DD)"),
+     *                 @OA\Property(property="address", type="string", description="Patient's address"),
+     *                 @OA\Property(property="mobile_no", type="string", description="Patient's mobile number"),
+     *                 @OA\Property(property="email", type="string", description="Patient's email address"),
+     *                 @OA\Property(property="id_type", type="string", description="ID type"),
+     *                 @OA\Property(property="id_number", type="string", description="ID number"),
+     *                 @OA\Property(property="arn_number", type="string", description="ARN number"),
+     *                 @OA\Property(property="marital_status", type="string", description="Marital status"),
+     *                 @OA\Property(property="primary_phone", type="string", description="Primary phone number"),
+     *                 @OA\Property(property="secondary_phone", type="string", description="Secondary phone number"),
+     *                 @OA\Property(property="home_phone", type="string", description="Home phone number"),
+     *                 @OA\Property(property="work_phone", type="string", description="Work phone number"),
+     *                 @OA\Property(
+     *                     property="languages",
+     *                     type="array",
+     *                     @OA\Items(type="string"),
+     *                     description="Languages spoken by the patient"
+     *                 ),
+     *                 @OA\Property(property="medical_aid", type="string", description="Medical aid"),
+     *                 @OA\Property(property="race", type="string", description="Race"),
+     *                 @OA\Property(property="ethnicity", type="string", description="Ethnicity"),
+     *                 @OA\Property(property="mrn_number", type="string", description="MRN number")
+     *             )
+     *         )
      *     ),
      *
      *     @OA\Response(
      *         response=201,
      *         description="Patient created successfully",
-     *
-     *         @OA\JsonContent(ref="#/components/schemas/PatientResource")
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Patient created successfully"),
+     *             @OA\Property(property="status", type="string", example="success"),
+     *             @OA\Property(property="statusCode", type="string", example="201"),
+     *             @OA\Property(property="data", ref="#/components/schemas/PatientResource"),
+     *         )
      *     ),
+     *     @OA\Response(response=422, ref="#/components/responses/422"),
+     *     @OA\Response(response=500, ref="#/components/responses/500")
      * )
      */
-    public function store(Request $request): JsonResponse
+
+    public function store(PatientCreateRequest $request): JsonResponse
     {
         DB::beginTransaction();
         try {
-            $validator = Validator::make($request->all(), [
-                'first_name' => 'required|string|max:255',
-                'last_name' => 'required|string|max:255',
-                'gender' => 'required|string|in:Male,Female,Other',
-                'dob' => 'required|date',
-                'address' => 'required|string',
-                'mobile_no' => 'nullable|string|max:20',
-                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
-                'id_type' => 'required|string',
-                'id_number' => 'required|string',
-                'arn_number' => 'required|string',
-                'marital_status' => 'required|string|in:Single,Married,Divorced,Widowed',
-                'primary_phone' => 'required|string|max:20',
-                'secondary_phone' => 'nullable|string|max:20',
-                'home_phone' => 'nullable|string|max:20',
-                'work_phone' => 'nullable|string|max:20',
-                'languages' => 'required|array',
-                'medical_aid'  => 'required|string',
-                'race' => 'nullable|string',
-                'ethnicity' => 'nullable|string',
-                'mrn_number' => 'nullable|string',
-            ]);
-
-            if ($validator->fails()) {
-                return response()->json($validator->errors(), 422);
-            }
-
             // User Creation
             $user = new User();
             $user->first_name = $request->first_name;
@@ -170,25 +197,60 @@ final class PatientController extends Controller
      *     summary="Show a specific patient",
      *     operationId="showPatient",
      *     tags={"Patients"},
+     *     description="Fetch details of a patient by their ID.",
      *
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
      *         required=true,
-     *
-     *         @OA\Schema(type="integer")
+     *         description="ID of the patient to show",
+     *         @OA\Schema(type="string", example="9d445a1c-cee5-4a68-b729-9edf8df71d87")
      *     ),
      *
      *     @OA\Response(
      *         response=200,
      *         description="Patient details",
-     *
-     *         @OA\JsonContent(ref="#/components/schemas/PatientResource")
+     *         @OA\JsonContent(
+     *              @OA\Property(property="message", type="string", example="Profile fetch successfully"),
+     *              @OA\Property(property="status", type="string", example="success"),
+     *              @OA\Property(property="statusCode", type="integer", example=200),
+     *              @OA\Property(property="data", type="object", ref="#/components/schemas/PatientResource"),
+     *          )
      *     ),
      *
-     *     @OA\Response(response=404, description="Patient not found")
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Unauthorized access"),
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="statusCode", type="integer", example=401)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=422,
+     *         description="Validation error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="Invalid patient ID"),
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="statusCode", type="integer", example=422)
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal server error",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="message", type="string", example="An error occurred on the server"),
+     *             @OA\Property(property="status", type="string", example="error"),
+     *             @OA\Property(property="statusCode", type="integer", example=500)
+     *         )
+     *     ),
+     *     security={
+     *         {"bearerAuth": {}}
+     *     }
      * )
      */
+
     public function show($id): JsonResponse
     {
         $patient = Patient::with('user', 'medicalHistories')->findOrFail($id);
@@ -205,29 +267,61 @@ final class PatientController extends Controller
      *     summary="Update a specific patient",
      *     operationId="updatePatient",
      *     tags={"Patients"},
+     *     description="Update the details of a patient by their ID.",
      *
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
      *         required=true,
-     *
+     *         description="ID of the patient to update",
      *         @OA\Schema(type="integer")
      *     ),
      *
      *     @OA\RequestBody(
      *         required=true,
-     *
-     *         @OA\JsonContent(ref="#/components/schemas/PatientUpdateRequest")
+     *         description="Updated patient data",
+     *         @OA\MediaType(
+     *              mediaType="application/json",
+     *              @OA\Schema(
+     *                  @OA\Property(property="first_name", type="string", description="Patient's first name"),
+     *                  @OA\Property(property="last_name", type="string", description="Patient's last name"),
+     *                  @OA\Property(property="gender", type="string", description="Patient's gender"),
+     *                  @OA\Property(property="dob", type="string", format="date", description="Date of birth (YYYY-MM-DD)"),
+     *                  @OA\Property(property="address", type="string", description="Patient's address"),
+     *                  @OA\Property(property="mobile_no", type="string", description="Patient's mobile number"),
+     *                  @OA\Property(property="email", type="string", description="Patient's email address"),
+     *                  @OA\Property(property="id_type", type="string", description="ID type"),
+     *                  @OA\Property(property="id_number", type="string", description="ID number"),
+     *                  @OA\Property(property="arn_number", type="string", description="ARN number"),
+     *                  @OA\Property(property="marital_status", type="string", description="Marital status"),
+     *                  @OA\Property(property="primary_phone", type="string", description="Primary phone number"),
+     *                  @OA\Property(property="secondary_phone", type="string", description="Secondary phone number"),
+     *                  @OA\Property(property="home_phone", type="string", description="Home phone number"),
+     *                  @OA\Property(property="work_phone", type="string", description="Work phone number"),
+     *                  @OA\Property(
+     *                      property="languages",
+     *                      type="array",
+     *                      @OA\Items(type="string"),
+     *                      description="Languages spoken by the patient"
+     *                  ),
+     *                  @OA\Property(property="medical_aid", type="string", description="Medical aid"),
+     *                  @OA\Property(property="race", type="string", description="Race"),
+     *                  @OA\Property(property="ethnicity", type="string", description="Ethnicity"),
+     *                  @OA\Property(property="mrn_number", type="string", description="MRN number")
+     *              )
+     *          )
      *     ),
      *
      *     @OA\Response(
      *         response=200,
      *         description="Patient updated successfully",
-     *
      *         @OA\JsonContent(ref="#/components/schemas/PatientResource")
      *     ),
      *
-     *     @OA\Response(response=404, description="Patient not found")
+     *     @OA\Response(
+     *         response=404,
+     *         description="Patient not found"
+     *     )
      * )
      */
     public function update(Request $request, $id): JsonResponse
@@ -260,22 +354,27 @@ final class PatientController extends Controller
      *     summary="Delete a specific patient",
      *     operationId="deletePatient",
      *     tags={"Patients"},
+     *     description="Delete a patient by their ID.",
      *
      *     @OA\Parameter(
      *         name="id",
      *         in="path",
      *         required=true,
-     *
-     *         @OA\Schema(type="integer")
+     *         description="ID of the patient to be deleted",
+     *         @OA\Schema(type="string")
      *     ),
      *
      *     @OA\Response(
      *         response=204,
      *         description="Patient deleted successfully"
      *     ),
-     *     @OA\Response(response=404, description="Patient not found")
+     *     @OA\Response(
+     *         response=404,
+     *         description="Patient not found"
+     *     )
      * )
      */
+
     public function destroy($id)
     {
         $patient = Patient::findOrFail($id);
--- src/modules/V1/Patient/Requests/PatientCreateRequest.php
+++ src/modules/V1/Patient/Requests/PatientCreateRequest.php
@@ -37,14 +37,26 @@ final class PatientCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
-            'mobile_no' => ['nullable','string','max:20'],
-            'gender' => ['required','string','max:50'],
-            'dob' => ['required','date'],
-            'profile_image' => ['nullable','string','max:2048'],
-            'address' => ['required','string','max:255'],
-            'arn_number' => ['required','string','max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'gender' => 'required|string|in:Male,Female,Other',
+            'dob' => 'required|date',
+            'address' => 'required|string',
+            'mobile_no' => 'nullable|string|max:20',
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
+            'id_type' => 'required|string',
+            'id_number' => 'required|string',
+            'arn_number' => 'required|string',
+            'marital_status' => 'required|string|in:Single,Married,Divorced,Widowed',
+            'primary_phone' => 'required|string|max:20',
+            'secondary_phone' => 'nullable|string|max:20',
+            'home_phone' => 'nullable|string|max:20',
+            'work_phone' => 'nullable|string|max:20',
+            'languages' => 'required|array',
+            'medical_aid' => 'required|string',
+            'race' => 'nullable|string',
+            'ethnicity' => 'nullable|string',
+            'mrn_number' => 'nullable|string',
         ];
 
     }
