c5d44ae6bf7565c204914c4ad991d6d1bc5ec325
Rachit Patel <raj4rachit@gmail.com>
2024-12-26 15:53:10 +0530

    packages changes

--- routes/v1/package.php
+++ routes/v1/package.php
@@ -3,13 +3,14 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
-use Modules\V1\Package\Controllers\SubscriptionController;
+use Modules\V1\Package\Controllers\PackageController;
+
 
 /**
  * Package's APIs
  **/
-Route::get('/', [SubscriptionController::class, 'index'])->name('package.index'); // List all packages
-Route::post('/', [SubscriptionController::class, 'store'])->name('package.store'); // Create a new package
-Route::get('/{id}', [SubscriptionController::class, 'show'])->name('package.show'); // Show a specific package
-Route::put('/{id}', [SubscriptionController::class, 'update'])->name('package.update'); // Update a specific package
-Route::delete('/{id}', [SubscriptionController::class, 'destroy'])->name('package.destroy'); // Delete a specific package
+Route::get('/', [PackageController::class, 'index'])->name('package.index'); // List all packages
+Route::post('/', [PackageController::class, 'store'])->name('package.store'); // Create a new package
+Route::get('/{id}', [PackageController::class, 'show'])->name('package.show'); // Show a specific package
+Route::put('/{id}', [PackageController::class, 'update'])->name('package.update'); // Update a specific package
+Route::delete('/{id}', [PackageController::class, 'destroy'])->name('package.destroy'); // Delete a specific package
